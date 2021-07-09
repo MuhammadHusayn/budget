c67ba2a21893799b120fa589da4537cc93257fa1
MuhammadHusayn <olimjonovm007@gmail.com>
2021-07-09 09:48:42 -0700

    finished

--- config.js
+++ config.js
@@ -2,11 +2,7 @@ const PORT = process.env.PORT || 4000
 const host = 'localhost'
 
 const pgConfig = {
-	user: 'postgres',
-	host: 'localhost',
-	port: 5432,
-	password: '2303',
-	database: 'budget'
+	connectionString: "postgres://uzrkjicn:kNXq7CXt3zirNBRl_HgeKPjta2AT2NY4@batyr.db.elephantsql.com/uzrkjicn"
 }
 
 module.exports = {
